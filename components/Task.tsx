f492ac25f3cc022d88de66eaca4770758066fdf2
Chris Voon <christopher.voon@gmail.com>
2025-05-18 13:29:01 -0700

    Fix tsconfig.json to not have to import React in every component file

--- components/Task.tsx
+++ components/Task.tsx
@@ -8,7 +8,7 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Task({
   text,
