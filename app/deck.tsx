e06fd04e2e67173eb200a62fb9d9850c1ecd2c58
Chris Voon <christopher.voon@gmail.com>
2025-06-03 23:01:49 -1000

    Replace SafeAreaView with View for layout in deck component

--- app/deck.tsx
+++ app/deck.tsx
@@ -1,7 +1,7 @@
 import Task from "@/components/Task";
 import ShakeToExit from "@/components/ShakeToExit";
 import { useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 export default function Index() {
   type TaskText = { text: string; id: number };
@@ -19,7 +19,7 @@ export default function Index() {
   }
 
   return (
-    <SafeAreaView className="flex-1">
+    <View className="flex-1">
       <ShakeToExit />
       {taskTexts.map((taskText) => {
         return (
@@ -31,6 +31,6 @@ export default function Index() {
           />
         );
       })}
-    </SafeAreaView>
+    </View>
   );
 }
