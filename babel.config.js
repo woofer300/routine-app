ac633f7f3e6c5481f382af32da35db22163d0c99
Chris Voon <christopher.voon@gmail.com>
2025-10-02 00:02:52 -0400

    Revert "Update packages and eslint/prettier config"
    This reverts commit 19ac1d1ca623ca5ff415e8046e91d7282ecfbd8d.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-import path from "path";
-const __dirname = path.dirname(__filename);
-
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
   };
 };
